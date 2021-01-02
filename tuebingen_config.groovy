9e4334f6592a0dd4ba8c1148a4bde33ec295896c
Tobias <masterarbeithhz@gmail.com>
2021-01-02 20:04:20 +0100

    sdgwsdg

--- tuebingen_config.groovy
+++ tuebingen_config.groovy
@@ -1,7 +1,7 @@
 env.UC_CUSTOMER="Tübingen"
-
 env.UC_DBNAME="mysquc-service"
 env.UC_DBUSER="root"
 env.UC_DBDB="testdb"
 env.UC_DBPSWD="Philipp1"
 env.UC_DOMAIN="http://hhz.regel-it.de"
+env.C_NAMESPACE="tue1"
